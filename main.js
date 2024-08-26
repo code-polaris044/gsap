d98013b455cd215be7859db1445645a578b98b47
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-08-26 19:55:52 +0900

    [fix]js&transition

--- main.js
+++ main.js
@@ -1,47 +1,71 @@
-gsap.registerPlugin(ScrollTrigger);
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.to(".box", {
-    x: 400,
-    scrollTrigger: {
-        trigger: ".container",
-        start: "top center",
-        markers: true,
-    },
-});
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".box", {
+//     x: 400,
+//     scrollTrigger: {
+//         trigger: ".container",
+//         start: "top center",
+//         markers: true,
+//     },
+// });
 
 // .textクラスの要素にアニメーションを適用
 // GSAPとScrollTriggerを登録
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 // matchMediaを使用してメディアクエリに基づくアニメーションを設定
-let mm = gsap.matchMedia();
+// let mm = gsap.matchMedia();
 
-mm.add("(max-width: 750px)", () => {
-    // 750px以下の画面幅のときのアニメーション
-    gsap.from(".text", {
-        opacity: 0,
-        x: -100, // 左から表示
-        duration: 1,
-        scrollTrigger: {
-            trigger: ".text",
-            start: "top 80%", // トリガーが発火するスクロール位置
-            markers: true, // デバッグ用のマーカーを表示
-            toggleActions: "play none none reverse",
-        },
-    });
-});
+// mm.add("(max-width: 750px)", () => {
+//     // 750px以下の画面幅のときのアニメーション
+//     gsap.from(".text", {
+//         opacity: 0,
+//         x: -100, // 左から表示
+//         duration: 1,
+//         scrollTrigger: {
+//             trigger: ".text",
+//             start: "top 80%", // トリガーが発火するスクロール位置
+//             markers: true, // デバッグ用のマーカーを表示
+//             toggleActions: "play none none reverse",
+//         },
+//     });
+// });
 
-mm.add("(min-width: 751px)", () => {
-    // 751px以上の画面幅のときのアニメーション
-    gsap.from(".text", {
-        opacity: 0,
-        y: 100, // 下から表示
+// mm.add("(min-width: 751px)", () => {
+//     // 751px以上の画面幅のときのアニメーション
+//     gsap.from(".text", {
+//         opacity: 0,
+//         y: 100, // 下から表示
+//         duration: 1,
+//         scrollTrigger: {
+//             trigger: ".text",
+//             start: "top 80%",
+//             markers: true,
+//             toggleActions: "play none none reverse",
+//         },
+//     });
+// });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(
+    ".box",
+    {
+        autoAlpha: 0,
+        y: 20,
+    },
+    {
+        autoAlpha: 1,
         duration: 1,
+        y: 0,
         scrollTrigger: {
-            trigger: ".text",
-            start: "top 80%",
+            trigger: ".box",
+            start: "top center",
             markers: true,
             toggleActions: "play none none reverse",
         },
-    });
-});
+    }
+);
